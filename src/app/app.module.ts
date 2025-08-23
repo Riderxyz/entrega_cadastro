cca402abbb53140dc9f2d2b569319a63083c2e74
Iago <ifavilla@gmail.com>
2025-08-23 13:12:26 -0300

    adicionando suporte para PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import ptBr from '@angular/common/locales/pt';
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,29 +16,42 @@ import {
 import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import {
-  getFirestore,
   initializeFirestore,
   persistentLocalCache,
   provideFirestore,
 } from '@angular/fire/firestore';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { registerLocaleData } from '@angular/common';
+import { ServiceWorkerModule } from '@angular/service-worker';
 registerLocaleData(ptBr);
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideAnalytics(() => getAnalytics()),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() =>
       initializeFirestore(getApp(), {
         localCache: persistentLocalCache(),
       })
     ),
+
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
-  providers: [provideHttpClient(), { provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    ScreenTrackingService,
+    UserTrackingService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
