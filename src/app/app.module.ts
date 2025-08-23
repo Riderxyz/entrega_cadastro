24d6d9062fa47cba0160935211b92d957fb8404e
Iago <ifavilla@gmail.com>
2025-08-23 11:39:39 -0300

    adicionando enviroments na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
+import ptBr from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { provideHttpClient } from '@angular/common/http';
+// Firebase
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getFirestore,
+  initializeFirestore,
+  persistentLocalCache,
+  provideFirestore,
+} from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment.prod';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(ptBr);
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() =>
+      initializeFirestore(getApp(), {
+        localCache: persistentLocalCache(),
+      })
+    ),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideHttpClient(), { provide: LOCALE_ID, useValue: 'pt-BR' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  loadMap: true,
+  firebaseConfig: {
+apiKey: "AIzaSyBsope_Ynvk0Fsc0Bb9AEH84JODdvrmcMw",
+  authDomain: "entregadashboard.firebaseapp.com",
+  projectId: "entregadashboard",
+  storageBucket: "entregadashboard.firebasestorage.app",
+  messagingSenderId: "566486294005",
+  appId: "1:566486294005:web:4ba843e7936c4d4e4394bc",
+  measurementId: "G-690Y70K0S9"
+  }
+};
